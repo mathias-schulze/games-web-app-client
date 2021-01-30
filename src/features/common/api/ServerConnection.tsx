b69f33552c96b82521ccc60bb5a47b7f948fe26f
Mathias Schulze <mathias-schulze@web.de>
2021-01-30 13:18:03 +0100

    read status on health check + clearIntervall fix

--- src/features/common/api/ServerConnection.tsx
+++ src/features/common/api/ServerConnection.tsx
@@ -1,20 +1,19 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { IconButton } from '@material-ui/core'
 import { Storage } from '@material-ui/icons'
 import api, { HEALTH_ENDPOINT } from './api'
 import { isConnected, setConnected } from './apiSlice'
 import { store } from '../store/store'
-import { Dispatch } from '@reduxjs/toolkit'
 
 function ServerConnection() {
 
-  const dispatch = useDispatch();
   const connected = useSelector(isConnected);
 
   useEffect(() => {
-    setInterval(() => checkServerHealth(dispatch), 10000);
-  });
+    const startHealthCheck = setInterval(() => checkServerHealth(), 10000);
+    return () => clearInterval(startHealthCheck);
+  }, []);
 
   return (
     <IconButton edge="end">
@@ -23,15 +22,20 @@ function ServerConnection() {
   )
 }
 
-const checkServerHealth = async (dispatch: Dispatch<any>) => {
+const checkServerHealth = async () => {
 
   const connected = store.getState().api.connected
+  const dispatch = store.dispatch;
 
   try {
-    await api.get(HEALTH_ENDPOINT);
-    if (connected !== true) {
-      dispatch(setConnected(true))
-    }
+    await api.get(HEALTH_ENDPOINT).then(response => {
+      const status = response.data.status
+      if (connected !== true && status === 'UP') {
+        dispatch(setConnected(true))
+      } else if (connected === true) {
+        dispatch(setConnected(false))
+      }
+    })
   } catch (error) {
     if (connected === true) {
       dispatch(setConnected(false))
