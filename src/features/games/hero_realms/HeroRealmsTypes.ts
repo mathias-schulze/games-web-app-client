e711409ace7000cff4fa0bf5b1ebb0a7c88fb857
Mathias Schulze <mathias-schulze@web.de>
2021-02-21 21:03:20 +0100

    added Hero Realms table data structure

---
+++ src/features/games/hero_realms/HeroRealmsTypes.ts
@@ -0,0 +1,44 @@
+export interface HeroRealmsTableView {
+  players: Player[];
+  cardBack: string;
+  fireGemsDeck: Deck;
+  market: Card[];
+  marketDeck: Deck;
+  sacrificePile: Deck;
+  ownPlayerArea: PlayerArea;
+  otherPlayerAreas: PlayerArea[];
+}
+
+export interface Player {
+	id: string;
+	name: string;
+}
+
+export interface Deck {
+  size: number;
+  cards: Card[];
+}
+
+export interface Card {
+  name: string;
+  cost: number;
+  defense: number;
+  faction: string;
+  type: string;
+  image: string;
+}
+
+export interface PlayerArea {
+  playerId: string;
+  playerName: string;
+	position: number;
+  health: number;
+  combat: number;
+  gold: number;
+  handSize: number;
+  hand: Card[];
+  deck: Deck;
+  discardPile: Deck;
+  playedCards: Card[];
+  champions: Card[];
+}
