790934b7eb5a2e38a6532f2d6cbca42af7954184
Mathias Schulze <mathias-schulze@web.de>
2021-03-12 19:28:49 +0100

    Hero Realms show empty played cards area

--- src/features/games/hero_realms/PlayedCards.tsx
+++ src/features/games/hero_realms/PlayedCards.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, makeStyles } from '@material-ui/core';
+import { Box, makeStyles, Paper } from '@material-ui/core';
 import { PlayerArea } from './HeroRealmsTypes'
 import { Card, playerColors } from './HeroRealmsTable';
 
@@ -8,6 +8,9 @@ export const useStyles = makeStyles({
     display: "flex",
     flexGrow: 1,
     width: "100%",
+  },
+  emptyCard: {
+    height: "166px",
   },
 });
 
@@ -24,6 +27,9 @@ function PlayedCards(props: PlayedCardsProps) {
   return (
     <Box className={classes.playedCards} justifyContent={props.justifyContent}
         style={{backgroundColor: playerColors[props.area.position]}}>
+      {props.area.playedCards.length === 0 &&
+        <Paper className={classes.emptyCard}/>
+      }
       {props.area.playedCards.map(card => {
         return (
           <Card key={"playedCard"+card.id} alt={card.name} image={card.image}
